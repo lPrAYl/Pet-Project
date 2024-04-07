76a65bd4df115838e6d10c2acd40adfb435922a5
Sergey Usenko <s.usenko@goodt.me>
2024-04-07 19:53:16 +0700

    refactoring

--- src/main/java/com/example/demo/controller/PetProjectController.java
+++ src/main/java/com/example/demo/controller/PetProjectController.java
@@ -5,23 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/")
+@RestController
+@RequestMapping("/")
 public class PetProjectController {
 
-//    private final EmailService emailService;
-//
-//    public PetProjectController(EmailService emailService) {
-//        this.emailService = emailService;
-//    }
-//
-//    @GetMapping("email")
-//    public void sendEmailWithVacancies() {
-//        emailService.sendEmail();
-//    }
-//
-//    @GetMapping("start")
-//    public String getString(){
-//        return "Это ответ от сервера";
-//    }
+    private final EmailService emailService;
+
+    public PetProjectController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("email")
+    public void sendEmailWithVacancies() {
+        emailService.sendEmail();
+    }
+
+    @GetMapping("start")
+    public String getString(){
+        return "Это ответ от сервера";
+    }
 }
